2d3d5ef8ac6675e987a04ff5bb428b88bbe66e55
Imayaselvan <imayaselvan@chargebee.com>
2022-07-15 17:23:43 +0530

    version bump.

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/amutha_c/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/amutha_c/Documents/GitHub/chargebee-flutter-new/example"
+export "FLUTTER_ROOT=/Users/cb-it-01-1737/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/cb-it-01-1737/Workspace New/Flutter/chargebee-flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/amutha_c/Documents/GitHub/chargebee-flutter-new/example/lib/main.dart"
+export "FLUTTER_TARGET=/Users/cb-it-01-1737/Workspace New/Flutter/chargebee-flutter/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/amutha_c/Documents/GitHub/chargebee-flutter-new/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/cb-it-01-1737/Workspace New/Flutter/chargebee-flutter/example/.dart_tool/package_config.json"
