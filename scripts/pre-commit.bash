fd1a975435ff4fca48a392e8cdfaeb3cce15ca19
cb-amutha <amutha.c@chargebee.com>
2023-01-12 18:11:32 +0530

    Added pre-commit hooks

---
+++ scripts/pre-commit.bash
@@ -0,0 +1,22 @@
+# Flutter formatter
+printf "\e[33;1m%s\e[0m\n" '=== Running Flutter Formatter ==='
+flutter format .
+
+hasNewFilesFormatted=$(git diff)
+if [ -n "$hasNewFilesFormatted" ]; then
+    git add .
+    printf "\e[33;1m%s\e[0m\n" 'Formmated files added to git stage'
+fi
+printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter Formatter'
+printf '%s\n' "${avar}"
+
+## Flutter Analyzer
+#printf "\e[33;1m%s\e[0m\n" '=== Running Flutter analyzer ==='
+#flutter analyze
+#if [ $? -ne 0 ]; then
+#  printf "\e[31;1m%s\e[0m\n" '=== Flutter analyzer error ==='
+#  pop_stash_files
+#  exit 1
+#fi
+#printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter analyzer'
+#printf '%s\n' "${avar}"
