d846472f74f08c275f76f91620e6a97a3ac3561b
cb-amutha <amutha.c@chargebee.com>
2023-02-27 11:22:42 +0530

    Removed github actions as taking it up separately

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -13,14 +13,3 @@ if [ -n "$stagedFiles" ]; then
 fi
 printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter Formatter'
 printf '%s\n' "${avar}"
-
-# Flutter Analyzer
-printf "\e[33;1m%s\e[0m\n" '=== Running Flutter analyzer ==='
-flutter analyze
-if [ $? -ne 0 ]; then
-  printf "\e[31;1m%s\e[0m\n" '=== Flutter analyzer error ==='
-  pop_stash_files
-  exit 1
-fi
-printf "\e[33;1m%s\e[0m\n" 'Finished running Flutter analyzer'
-printf '%s\n' "${avar}"
