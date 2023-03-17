2fe83fef2979c8ec3fd0e4d99be4d8ff0409a052
cb-amutha <amutha.c@chargebee.com>
2023-03-17 15:55:30 +0530

    added integration test for sdk methods

--- example/integration_test/chargebee_test.dart
+++ example/integration_test/chargebee_test.dart
@@ -7,13 +7,8 @@ import 'package:chargebee_flutter_sdk_example/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  final String siteName = "cb-test";
-  final String apiKey = "test_****";
-  final String iosSDKKey = "cb-*****";
-  final String androidSDKKey = "cb-*****";
-
   ChargebeeTest chargebeeTest;
+
   group('end-to-end-test', () {
     testWidgets('Chargebee sdk test', (tester) async {
       app.main();
@@ -24,6 +19,10 @@ void main() {
       await chargebeeTest.retrieveProductIdentifiersWithoutParam();
       await chargebeeTest.retrieveProductIdentifiersWithParam();
       await chargebeeTest.retrieveProducts();
+      await chargebeeTest.retrieveSubscriptions();
+      await chargebeeTest.retrieveEntitlements();
+      await chargebeeTest.retrieveAllItems();
+      await chargebeeTest.retrieveAllPlans();
     });
   });
 }
@@ -33,16 +32,16 @@ class ChargebeeTest {
   ChargebeeTest(this.tester);
 
   Future<void> configureAndLaunchApp() async {
-    final String siteName = "cb-imay-test";
-    final String apiKey = "test_EojsGoGFeHoc3VpGPQDOZGAxYy3d0FF3";
-    final String iosSDKKey = "cb-njjoibyzbrhyjg7yz4hkwg2ywq";
-    final String androidSDKKey = "cb-wpkheixkuzgxbnt23rzslg724y";
+    final String siteName = "cb-test";
+    final String apiKey = "test_****";
+    final String iosSDKKey = "cb-*****";
+    final String androidSDKKey = "cb-*****";
     await tester.pumpAndSettle();
     try {
       tester.printToConsole('Start configuring app with chargebee');
       await Chargebee.configure(siteName, apiKey, iosSDKKey, androidSDKKey);
       tester.printToConsole('Configuration done successfully!');
-    } on PlatformException catch(e){
+    } on PlatformException catch (e) {
       fail('Error message: ${e.message}');
     }
     expect(find.text('Configure'), findsOneWidget);
@@ -55,41 +54,115 @@ class ChargebeeTest {
   }
 
   Future<void> retrieveProductIdentifiersWithoutParam() async {
-    tester.printToConsole('Fetch store specific products from chargebee');
+    tester.printToConsole('Fetch store specific product Ids from chargebee');
     try {
       final list = await Chargebee.retrieveProductIdentifers();
       expect(list.isNotEmpty, true);
-      await tester.pumpAndSettle(Duration(seconds: 10));
-    } on PlatformException catch(e){
+      tester.printToConsole('Product Ids retrieved successfully!');
+    } on PlatformException catch (e) {
       fail('Error: ${e.message}');
     }
-    tester.printToConsole('Store specific products retrieved successfully!');
   }
 
   Future<void> retrieveProductIdentifiersWithParam() async {
-    tester.printToConsole('Fetch store specific products from chargebee with params');
+    tester.printToConsole(
+        'Fetch store specific product Ids from chargebee with params');
     try {
-      final list = await Chargebee.retrieveProductIdentifers({"limit":"10"});
+      final list = await Chargebee.retrieveProductIdentifers({"limit": "10"});
+      debugPrint('list: $list');
       expect(list.isNotEmpty, true);
-      await tester.pumpAndSettle(Duration(seconds: 10));
-    } on PlatformException catch(e){
+      tester.printToConsole('Product Ids retrieved successfully!');
+    } on PlatformException catch (e) {
       fail('Error: ${e.message}');
     }
-
-    tester.printToConsole('Store specific products retrieved successfully!');
   }
 
   Future<void> retrieveProducts() async {
     tester.printToConsole('Fetch product details from store(apple or google)');
-      List<String> productList = ['merchant.pro.android'];
-      try {
-        final list = await Chargebee.retrieveProducts(productList);
-        debugPrint('list: $list');
-        expect(list.isNotEmpty, true);
-      } on PlatformException catch(e){
-        fail('Error: ${e.message}');
-      }
-    tester.printToConsole('Products retrieved from store successfully!');
 
+    /// Need to pass the list of product ids based on stores
+    //List<String> productList = ['chargebee.pro.ios']; ///'chargebee.pro.ios'
+    List<String> productList = ['merchant.pro.android'];
+    try {
+      final product = await Chargebee.retrieveProducts(productList);
+      debugPrint('Product : $product');
+      expect(product.isNotEmpty, true);
+      tester.printToConsole('Products retrieved successfully!');
+    } on PlatformException catch (e) {
+      fail('Error: ${e.message}');
+    }
+  }
+
+  Future<void> purchaseProducts() async {
+    tester.printToConsole('Starting to subscribe the product');
+
+    Product product = new Product(
+        "merchant.pro.android",
+        0.0,
+        'priceString',
+        'title',
+        'currencyCode',
+        SubscriptionPeriod.fromMap(
+            {"periodUnit": "month", "numberOfUnits": 3}));
+    try {
+      final result = await Chargebee.purchaseProduct(product, 'abc');
+      debugPrint('purchase result: $result');
+      expect(result != null, true);
+      tester.printToConsole('Product subscribed successfully!');
+    } on PlatformException catch (e) {
+      fail('Error: ${e.message}');
+    }
+  }
+
+  Future<void> retrieveSubscriptions() async {
+    tester.printToConsole('Start retrieving subscriptions');
+    try {
+      final subscriptions = await Chargebee.retrieveSubscriptions(
+          {"channel": "play_store", "customer_id": "abc"});
+      debugPrint('Subscriptions: $subscriptions');
+      expect(subscriptions.isNotEmpty, true);
+      tester.printToConsole('Subscription retrieved successfully!');
+    } on PlatformException catch (e) {
+      fail('Error: ${e.message}');
+    }
+  }
+
+  Future<void> retrieveEntitlements() async {
+    tester.printToConsole('Start retrieving entitlements');
+    try {
+      final entitlements = await Chargebee.retrieveEntitlements(
+          {"subscriptionId": "AzZlGJTC9U3tw4nF"});
+      debugPrint('Entitlements : $entitlements');
+      expect(entitlements.isNotEmpty, true);
+      tester.printToConsole('Entitlements retrieved successfully!');
+    } on PlatformException catch (e) {
+      fail('Error: ${e.message}');
+    }
+  }
+
+  Future<void> retrieveAllItems() async {
+    tester.printToConsole('Start retrieving all items');
+    try {
+      final subscriptions = await Chargebee.retrieveAllItems(
+          {"limit": "5", "channel[is]": "play_store"});
+      debugPrint('Items : $subscriptions');
+      expect(subscriptions.isNotEmpty, true);
+      tester.printToConsole('Items retrieved successfully!');
+    } on PlatformException catch (e) {
+      fail('Error: ${e.message}');
+    }
+  }
+
+  Future<void> retrieveAllPlans() async {
+    tester.printToConsole('Start retrieving all plans');
+    try {
+      final subscriptions = await Chargebee.retrieveAllPlans(
+          {"limit": "5", "channel[is]": "app_store"});
+      debugPrint('Plans : $subscriptions');
+      expect(subscriptions.isNotEmpty, true);
+      tester.printToConsole('Plans retrieved successfully!');
+    } on PlatformException catch (e) {
+      fail('Error: ${e.message}');
+    }
   }
 }
